eb881559441bdae194d70f96157b1a440ae9bb65
adi.lewenstein <adi.lewenstein@sisense.com>
2019-07-24 19:12:42 +0300

    Add Emoji to Telegram

--- src/utils/telegram.js
+++ src/utils/telegram.js
@@ -1,11 +1,12 @@
 const chalk = require('chalk');
 const Telegram = require('telegram-bot-api');
+const emoji = require('node-emoji');
 const bot = new Telegram({ token: '823158626:AAFrdjYf-UFBGN5uZRmnvU3MhI5JVY89GEk' })
 
 
 bot.sendMessage({
     chat_id: '-371062059',
-    text: 'Hiiiiiiii :eggplant:'
+    text: emoji.emojify('Hiiiiiiii :heart:')
 }).then(
     (res) => console.log(res),
     (err) => console.log(chalk.red(`Error ${err.error_code}: ${err.description}`))
