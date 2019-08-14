c13c318f2817317898b1d35569d0df54b1b957a3
adi.lewenstein <adi.lewenstein@sisense.com>
2019-08-14 16:22:07 +0300

    Hefker

--- src/utils/telegram2.js
+++ src/utils/telegram2.js
@@ -4,6 +4,9 @@ const Telegraf = require('telegraf');
 const chalk = require('chalk');
 const yad2Api = require('./yad2Api');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
 const token = '823158626:AAFrdjYf-UFBGN5uZRmnvU3MhI5JVY89GEk'
 const bot = new Telegraf(token)
 
@@ -14,41 +17,52 @@ const dataPath = path.join(dataFileDir, 'appartments.json')
 
 
 bot.on('sticker', (ctx) => {
-    const {file_id} = ctx.message.sticker
+    const {
+        file_id
+    } = ctx.message.sticker
     ctx.reply(file_id)
 })
-
-
 
 bot.command('/getreportnow', (ctx) => {
     console.log(ctx)
     ctx.reply('Working on it');
     //yad2Api.getApptsList( {}, true)
-    const {queryFilters, customFilters} = yad2Api.getDefaultFilters();
+    const {
+        queryFilters,
+        customFilters
+    } = yad2Api.getDefaultFilters();
     yad2Api.getApptsList(queryFilters)
-        .then( 
-            ({appartments}) =>{
-                const prevQueryResultBuffer = fs.readFileSync(dataPath) ||'';
+        .then(
+            ({
+                appartments
+            }) => {
+                const prevQueryResultBuffer = fs.readFileSync(dataPath) || '';
                 const prevQueryResultJson = prevQueryResultBuffer.toString();
                 const prevResult = JSON.parse(prevQueryResultJson);
 
-                const newAppts = appartments.filter(({id})=>{
-                    return !prevResult.some(a=> a.id === id)
+                const newAppts = appartments.filter(({
+                    id
+                }) => {
+                    return !prevResult.some(a => a.id === id)
                 })
-                const message = newAppts.map(apt=> apt.id)
+                const message = newAppts.map(apt => apt.id)
 
-                fs.writeFileSync( dataPath , JSON.stringify(appartments))
+                fs.writeFileSync(dataPath, JSON.stringify(appartments))
 
                 return ctx.reply(message)
             },
-            err =>console.log(err)
-            )
-    
+            err => console.log(err)
+        )
+
 
 })
 
 bot.hears('Love You', (ctx) => {
     ctx.reply('Love you too!')
     ctx.replyWithSticker('CAADAgADQgcAAowt_QdHeoWouiAFCBYE')
+})
+
+bot.hears('הפקר', (ctx) => {
+    ctx.replyWithSticker('CAADBAADlgAD9csoBWoiHhtQmQrtFgQ')
 })
 module.exports = bot;
